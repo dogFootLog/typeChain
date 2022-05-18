a9b972a9722c59711890c364e96f3a65fef00a0c
dogfoot <dogfoot_log@naver.com>
2022-05-18 23:34:20 +0900

    [220518] typeChain DONE

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,55 @@
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "hi;";
+import crypto from "crypto";
+
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class Blockchian {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchian = new Blockchian();
+
+blockchian.addBlock("First one");
+blockchian.addBlock("Second one");
+blockchian.addBlock("Third one");
+
+blockchian.getBlocks().push(new Block("xxxxx", 1111, "Hackeddddd"));
+
+console.log(blockchian.getBlocks());
---
+++ src/index_old.ts
@@ -0,0 +1,25 @@
+/*
+class Block {
+  constructor(private data: string) {}
+  static hello() {
+    return "hi;";
+  }
+}
+*/
+
+/*
+import { init, exit } from "myPackage";
+
+init({ url: "true" });
+exit(1);
+*/
+/*
+import { init, exit } from "./myPackage.js";
+
+init({
+  url: "true",
+  debug: false,
+});
+exit(1);
+*/
+console.log("bye");
